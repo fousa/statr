9d297f9757166ca07a8b078ab56ab14a994407d0
Jelle Vandebeeck <jelle@fousa.be>
2011-01-27 21:57:42 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Statr::Application.config.secret_token = 'fa1c40002c484f62c14f561d6ad6b399d162f6ed190341a7973422eaf32570ad835cd0d40add97ba52068369aca5aa7f43ec5d671631845a8163ab331e0893fc'
