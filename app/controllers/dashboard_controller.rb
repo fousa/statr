9d268d2ec5d7e46b70dbdace308d8b9b3210fa83
Jelle Vandebeeck <jelle@fousa.be>
2011-01-31 23:20:28 +0100

    list of recent transactions on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def show
+    @transactions = current_user.transactions.order("executed_at DESC").limit(10)
   end
 end
