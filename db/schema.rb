14aa49db54315d6b9960e448e75062f28539aa36
Jelle Vandebeeck <jelle@fousa.be>
2011-01-31 23:12:57 +0100

    add test for category deletion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110127221757) do
+ActiveRecord::Schema.define(:version => 20110130154457) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -18,7 +18,20 @@ ActiveRecord::Schema.define(:version => 20110127221757) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "cached_slug"
   end
+
+  create_table "slugs", :force => true do |t|
+    t.string   "name"
+    t.integer  "sluggable_id"
+    t.integer  "sequence",                     :default => 1, :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "slugs", ["name", "sluggable_type", "sequence", "scope"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
+  add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
 
   create_table "transactions", :force => true do |t|
     t.string   "description"
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,4 +6,25 @@ describe Category do
     should have_many(:transactions)
     should validate_presence_of(:name)
   }
+
+  it "can not be deleted when transactions available" do
+    user = User.create(:email => "jelle@fousa.be", :password => "123123", :password_confirmation => "123123")
+    category = Category.create(:name => "test")
+    user.categories << category
+    transaction = Transaction.create(:executed_at => Date.today, :amount => 10)
+    category.transactions << transaction
+    category.transactions.count.should equal 1
+
+    category.destroy
+    Category.count.should equal 1
+  end
+
+  it "can be deleted when transactions available" do
+    user = User.create(:email => "jelle@fousa.be", :password => "123123", :password_confirmation => "123123")
+    category = Category.create(:name => "test")
+    user.categories << category
+
+    category.destroy
+    Category.count.should equal 0
+  end
 end
