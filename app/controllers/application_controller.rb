395601485718e1739abf90fe70ffa6531d16466d
Jelle Vandebeeck <jelle@fousa.be>
2011-02-03 19:18:04 +0100

    fix bug with current date

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_date(year = nil, month = nil)
-    return Time.now if year.nil? || month.nil?
+    return Time.zone.now if year.nil? || month.nil?
 
-    Time.local(year, month)
+    Time.zone.local(year, month)
   end
 end
